c316f51cc26ccb1a88b36cfa13006419fd45d841
Dylan Camus <dylancamus93@gmail.com>
2017-12-04 14:02:19 -0800

    Created publiic and private subnets with appropriate route tables

--- aws/wordpress/terraform/subnet-variables.tf
+++ aws/wordpress/terraform/subnet-variables.tf
@@ -1,11 +1,21 @@
 variable "subnet_1_cidr" {
-    default     = "10.0.1.0/24"
+    default     = "10.0.0.0/24"
     description = "Subnet 1"
 }
 
 variable "subnet_2_cidr" {
-    default     = "10.0.2.0/24"
+    default     = "10.0.1.0/24"
     description = "Subnet 2"
+}
+
+variable "subnet_3_cidr" {
+    default     = "10.0.2.0/24"
+    description = "Subnet 3"
+}
+
+variable "subnet_4_cidr" {
+    default     = "10.0.3.0/24"
+    description = "Subnet 4"
 }
 
 variable "az_1" {
