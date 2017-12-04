7f31e24f03725cab30b06e9e826ea06ef76044ad
Dylan Camus <dylan.camus@skyward.io>
2017-12-04 14:07:50 -0800

    Added SSH access to web and db SGs

--- aws/wordpress/terraform/sg.tf
+++ aws/wordpress/terraform/sg.tf
@@ -4,10 +4,17 @@ resource "aws_security_group" "web_sg" {
     vpc_id      = "${aws_vpc.main.id}"
 
     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port   = 80
         to_port     = 80
         protocol    = "TCP"
-        cidr_blocks = ["${var.cidr_blocks}"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
@@ -28,10 +35,17 @@ resource "aws_security_group" "db_sg" {
     vpc_id      = "${aws_vpc.main.id}"
 
     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port   = 3389
         to_port     = 3389
         protocol    = "TCP"
-        cidr_blocks = ["${var.cidr_blocks}"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
