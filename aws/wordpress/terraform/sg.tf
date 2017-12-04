89e721cea6d1c2313eea253736952cd0645a3a9c
Dylan Camus <dylan.camus@skyward.io>
2017-12-04 15:59:20 -0800

    Updated security groups

--- aws/wordpress/terraform/sg.tf
+++ aws/wordpress/terraform/sg.tf
@@ -1,20 +1,42 @@
+# Public HTTP access for ELB
+resource "aws_security_group" "web_elb_sg" {
+  name        = "web_elb_sg"
+  description = "Allows public HTTP inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Private HTTP access and SSH from anywhere for web instances
 resource "aws_security_group" "web_sg" {
     name        = "main_web_sg"
-    description = "Allow HTTP inbound traffic"
+    description = "Allow public SSH and private HTTP inbound traffic"
     vpc_id      = "${aws_vpc.main.id}"
 
     ingress {
         from_port   = 22
         to_port     = 22
-        protocol    = "TCP"
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 80
         to_port     = 80
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "tcp"
+        cidr_blocks = ["${aws_vpc.main.cidr_block}"]
     }
 
     egress {
@@ -29,23 +51,24 @@ resource "aws_security_group" "web_sg" {
     }
 }
 
+# SSH and RDS access for db instances
 resource "aws_security_group" "db_sg" {
     name        = "main_db_sg"
-    description = "Allow RDS inbound traffic"
+    description = "Allow private RDS and public SSH inbound traffic"
     vpc_id      = "${aws_vpc.main.id}"
 
     ingress {
         from_port   = 22
         to_port     = 22
-        protocol    = "TCP"
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 3389
         to_port     = 3389
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "tcp"
+        cidr_blocks = ["${aws_vpc.main.cidr_block}"]
     }
 
     egress {
