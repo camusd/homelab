fa278170993c9c2753e27add300dbdb87450f539
Dylan Camus <dylancamus93@gmail.com>
2017-12-04 19:42:20 -0800

    renamed aws variables and added additional wp variables

--- aws/wordpress/terraform/web-user-data.tpl
+++ aws/wordpress/terraform/web-user-data.tpl
@@ -3,6 +3,14 @@ echo "export TF_VAR_db_name=${db_name}" >> /etc/profile
 echo "export TF_VAR_db_username=${db_username}" >> /etc/profile
 echo "export TF_VAR_db_password=${db_password}" >> /etc/profile
 echo "export DB_HOST=${db_host}" >> /etc/profile
+echo "export AUTH_KEY=${wp_auth_key}" >> /etc/profile
+echo "export SECURE_AUTH_KEY=${wp_secure_auth_key}" >> /etc/profile
+echo "export LOGGED_IN_KEY=${wp_logged_in_key}" >> /etc/profile
+echo "export NONCE_KEY=${wp_nonce_key}" >> /etc/profile
+echo "export AUTH_SALT=${wp_auth_salt}" >> /etc/profile
+echo "export SECURE_AUTH_SALT=${wp_secure_auth_salt}" >> /etc/profile
+echo "export LOGGED_IN_SALT=${wp_logged_in_salt}" >> /etc/profile
+echo "export NONCE_SALT=${wp_nonce_salt}" >> /etc/profile
 sudo yum -y update
 sudo yum -y install httpd
 sudo yum -y install git
--- aws/wordpress/terraform/web-variables.tf
+++ aws/wordpress/terraform/web-variables.tf
@@ -1,4 +1,4 @@
-variable "public_key_path" {
+variable "aws_public_key_path" {
   description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
@@ -7,10 +7,42 @@ Example: ~/.ssh/terraform.pub
 DESCRIPTION
 }
 
-variable "key_name" {
+variable "aws_key_name" {
   description = "Desired name of AWS key pair"
 }
 
 variable "git_repo" {
   description = "The repo that contains the wordpress app"
+}
+
+variable "wp_auth_key" {
+  description = "Wordpress auth key"
+}
+
+variable "wp_secure_auth_key" {
+  description = "Wordpress secure auth key"
+}
+
+variable "wp_logged_in_key" {
+  description = "Wordpress logged in key"
+}
+
+variable "wp_nonce_key" {
+  description = "Wordpress nonce key"
+}
+
+variable "wp_auth_salt" {
+  description = "Wordpress auth salt"
+}
+
+variable "wp_secure_auth_salt" {
+  description = "Wordpress secure auth salt"
+}
+
+variable "wp_logged_in_salt" {
+  description = "Wordpress logged in salt"
+}
+
+variable "wp_nonce_salt" {
+  description = "Wordpress nonce salt"
 }
